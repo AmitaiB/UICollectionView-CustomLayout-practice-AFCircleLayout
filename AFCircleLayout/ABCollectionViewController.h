c49410d88682e8c054035c65ab333f3ef77966a0
AmitaiB <blickstein@gmail.com>
2015-10-15 15:03:23 -0400

    Adds empty xcode project. Adds CollectionView

---
+++ AFCircleLayout/ABCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABCollectionViewController.h
+//  AFCircleLayout
+//
+//  Created by Amitai Blickstein on 10/15/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCollectionViewController : UICollectionViewController
+
+@end
---
+++ AFCircleLayout/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  AFCircleLayout
+//
+//  Created by Amitai Blickstein on 10/14/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) UINavigationController *viewController;
+
+@end
+
